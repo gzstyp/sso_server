584de85cbfe71c85929b214f9f7b3565fb700122
gzstyp <444141300@qq.com>
2020-06-07 11:52:57 +0800

    删除空格

--- web1/src/main/java/com/fwtai/web1/controller/Web1Controller.java
+++ web1/src/main/java/com/fwtai/web1/controller/Web1Controller.java
@@ -4,7 +4,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class Web1Controller{
 
